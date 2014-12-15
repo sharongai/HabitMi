37bb917a46ac9893d7b5b5fee848645d31d08751
Chris Jeon <chris0374@gmail.com>
2014-12-15 00:45:54 -0500

    Adds home_controller specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'capybara/poltergeist'
 
+SimpleCov.start
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
