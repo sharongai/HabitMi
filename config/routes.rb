080ecd6e65a66221267ef888b12890777422d668
Sharon Gai <sharongai9@gmail.com>
2014-12-14 23:38:20 -0500

    aboutMe

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/about' => 'home#about'
+  get '/contact' => 'home#contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
