ebb0c620eb09dd2b0957c8d41aa6dd3a7519a8b7
Sharon Gai <sharongai9@gmail.com>
2014-12-14 23:57:18 -0500

    whateverIdid

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get '/about' => 'home#about'
   get '/contact' => 'home#contact'
+  get '/peoplesgoals'=> 'home#peoplesgoals'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
