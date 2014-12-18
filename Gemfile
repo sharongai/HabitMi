a254667aad107d00b32c6346555b8c9c6c639eae
Chris Jeon <chris0374@gmail.com>
2014-12-17 23:01:36 -0500

    Finishes Landing Page

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.1.3'
 
 gem 'devise'
 gem 'dotenv-rails', :groups => [:development, :test]
+gem "font-awesome-rails"
 gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
 gem 'omniauth-facebook'
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('div.explanation-carousel a.icon-link').on('click', function(e) {
+    e.preventDefault();
+    $('div.icons div.columns').removeClass('active');
+    $(this).closest('div.three.columns').addClass('active');
+    $('div.explanations').removeClass('active');
+
+    switch ($(this).attr('class')) {
+      case 'icon-link our-mission':
+        $('div.explanations.our-mission').addClass('active');
+        break;
+      case 'icon-link trending-goals':
+        $('div.explanations.trending-goals').addClass('active');
+        break;
+      case 'icon-link how-to-habit-mi':
+        $('div.explanations.how-to-habit-mi').addClass('active');
+        break;
+      case 'icon-link win-prizes':
+        $('div.explanations.win-prizes').addClass('active');
+        break;
+    }
+  });
+});
