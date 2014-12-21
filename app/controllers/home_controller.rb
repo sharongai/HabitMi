cd61e583e1c0b490a92473ff49f59293bab20473
Chris Jeon <chris0374@gmail.com>
2014-12-20 23:52:24 -0500

    Fixes issue where goal list is blank if search entry in blank

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,12 @@ class HomeController < ApplicationController
 
   def search_goals
     respond_to do |format|
-      @goals = Goal.search_by_title(params[:goal_title])
+      if params[:goal_title].blank?
+        @goals = Goal.where(
+          created_at: Time.now.beginning_of_day..Time.now.end_of_day)
+      else
+        @goals = Goal.search_by_title(params[:goal_title])
+      end
 
       format.json do
         render json: {
