4e6ec2c694bbc1bd54437066b01bf9f3d0561df0
Kai Wang <jesuiswk@gmail.com>
2014-12-22 23:10:43 -0500

    css

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-
-$("#edit_title").click(function(){
-    $("#goal_title").focus();
-  });
+	$("#edit_title").click(function(){
+		$("#goal_title").focus();
+	});
+	$(".add_user").click(function(e){
+		$(e.target).toggleClass("added");
+	});
 });
